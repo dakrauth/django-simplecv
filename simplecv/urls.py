6e1e5b369429a055867691b0ca3ae68e7e0f8cb0
David Krauth <dakrauth@gmail.com>
2021-03-10 16:09:39 -1000

    Updates for Django 3.1

--- simplecv/urls.py
+++ simplecv/urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls import url
+from django.urls import re_path
 from . import views
 
 app_name = 'simplecv'
 
 urlpatterns = [
-    url(r'^$', views.cv_view, name='simplecv'),
-    url(r'^cv\.(\w+)$', views.cv_view, name='simplecv-format'),
-    url(r'^docx/$', views.docx_view, name='docx-view'),
-    url(r'^docx/html/$', views.docx_html_view, name='docx-html-view'),
+    re_path(r'^$', views.cv_view, name='simplecv'),
+    re_path(r'^cv\.(\w+)$', views.cv_view, name='simplecv-format'),
+    re_path(r'^docx/$', views.docx_view, name='docx-view'),
+    re_path(r'^docx/html/$', views.docx_html_view, name='docx-html-view'),
 ]
