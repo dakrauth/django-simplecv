e41a8f502f7910ef76fee24f9fc57214aba765f4
David Krauth <dakrauth@gmail.com>
2024-11-26 22:18:26 -0800

    Switched to pyproject.toml build

--- simplecv/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Simple CV/Résumé generator and exporter.
-"""
-
-VERSION = (0, 2)
-
-
-def get_version():
-    return ".".join(map(str, VERSION))
---
+++ src/simplecv/__init__.py
@@ -0,0 +1,9 @@
+"""
+Simple CV/Résumé generator and exporter.
+"""
+__version__ = "0.3"
+VERSION = tuple(int(i) for i in __version__.split("."))
+
+
+def get_version():
+    return __version__
